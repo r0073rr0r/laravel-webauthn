89e33c812e640911d57dd32ecf75dcce0af63c93
Velimir Majstorov <majstorov@gmail.com>
2025-11-07 14:03:10 +0100

    test: Add mock support for openssl_pkey_get_public() in tests
    - Added mock for openssl_pkey_get_public() to support new login flow
    - Added mock for openssl_free_key() for test cleanup
    - Updated OpenSslMock to return mock resource for getPublicKey()
    - Fixes test failure after adding openssl_pkey_get_public() validation

--- tests/Support/overrides.php
+++ tests/Support/overrides.php
@@ -10,4 +10,21 @@ if (!function_exists(__NAMESPACE__ . '\\openssl_verify')) {
     }
 }
 
+// Override openssl_pkey_get_public for tests
+if (!function_exists(__NAMESPACE__ . '\\openssl_pkey_get_public')) {
+    function openssl_pkey_get_public($public_key)
+    {
+        return \r0073rr0r\WebAuthn\Tests\Support\OpenSslMock::getPublicKey($public_key);
+    }
+}
+
+// Override openssl_free_key for tests
+if (!function_exists(__NAMESPACE__ . '\\openssl_free_key')) {
+    function openssl_free_key($key_identifier)
+    {
+        // No-op in tests
+        return true;
+    }
+}
+
 
